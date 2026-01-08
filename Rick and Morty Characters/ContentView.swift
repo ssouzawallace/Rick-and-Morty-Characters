05dde41825d2b7bd7b49ed80ff1bf3578fb59fce
Wallace Souza Silva <ssouza.wallace@icloud.com>
2026-01-08 20:03:56 -0300

    Initial Commit

---
+++ Rick and Morty Characters/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rick and Morty Characters
+//
+//  Created by Wallace Souza Silva on 08/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rick and Morty CharactersUITests/Rick_and_Morty_CharactersUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Rick_and_Morty_CharactersUITestsLaunchTests.swift
+//  Rick and Morty CharactersUITests
+//
+//  Created by Wallace Souza Silva on 08/01/26.
+//
+
+import XCTest
+
+final class Rick_and_Morty_CharactersUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
