698e3393c99c95bbe0eb1a27695795a8a512c516
Wallace Souza Silva <ssouza.wallace@icloud.com>
2026-01-08 20:03:56 -0300

    Initial Commit

---
+++ Rick and Morty Characters/Rick_and_Morty_CharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_Morty_CharactersApp.swift
+//  Rick and Morty Characters
+//
+//  Created by Wallace Souza Silva on 08/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Rick_and_Morty_CharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Rick and Morty CharactersTests/Rick_and_Morty_CharactersTests.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_Morty_CharactersTests.swift
+//  Rick and Morty CharactersTests
+//
+//  Created by Wallace Souza Silva on 08/01/26.
+//
+
+import Testing
+@testable import Rick_and_Morty_Characters
+
+struct Rick_and_Morty_CharactersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
